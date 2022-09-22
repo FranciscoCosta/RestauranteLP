387f2681f21759a64ae2f25018f24ae46010c75b
Francisco Costa <francisco100eg@gmail.com>
2022-09-22 14:14:24 -0300

    : Adicionado section Chef / Galeria / Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, Footer, Gallery, Header, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -11,10 +11,7 @@ const App = () => (
     <AboutUs />
     <SpecialMenu />
     <Chef />
-    <Intro />
-    <Laurels />
     <Gallery />
-    <FindUs />
     <Footer />
   </div>
 );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contatos</h1>
+        <p className="p__opensans">Belo Horizonte , Minas Gerais</p>
+        <p className="p__opensans">+31 111111111</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.logoP} alt="footer_logo" />
+        <p className="p__opensans">&quot;A melhor comida portuguesa no Brasil.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Horário</h1>
+        <p className="p__opensans">Segunda a sexta:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Sábado e Domingo:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Francisco Costa. Todos os direitos Reservados</p>
+    </div>
+
   </div>
 );
 
